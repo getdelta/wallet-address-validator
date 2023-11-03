3de8c7d4d1f87da2de1cfe3c7e5f7627dfce8c72
segersniels <segers.n@hotmail.com>
2023-11-03 09:30:44 +0100

    :sparkles: Add very basic Neo N3 wallet validation

--- src/currencies.js
+++ src/currencies.js
@@ -24,6 +24,7 @@ var STEEMValidator = require("./steem_validator");
 var SYSValidator = require("./sys_validator");
 var ZILValidator = require("./zil_validator");
 var NXTValidator = require("./nxt_validator");
+var N3Validator = require("./n3_validator");
 
 // defines P2PKH, P2SH and bech32 address types for standard (prod) and testnet networks
 var CURRENCIES = [
@@ -287,6 +288,11 @@ var CURRENCIES = [
         symbol: "gas",
         addressTypes: { prod: ["17"], testnet: [] },
         validator: BTCValidator,
+    },
+    {
+        name: "Neo N3",
+        symbol: "n3",
+        validator: N3Validator,
     },
     {
         name: "Qtum",
